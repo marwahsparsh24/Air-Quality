d4136f3decf194d354c9820006bfd8a704633a16
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2024-11-12 14:11:03 -0500

    alerts

--- dags/dag_script_model.py
+++ dags/dag_script_model.py
@@ -25,6 +25,31 @@ from dags.bestmodel import main as bestmodel
 conf.set('core', 'enable_xcom_pickling', 'True')
 conf.set('core', 'enable_parquet_xcom', 'True')
 
+def failure_email_notification(context):
+    """Send failure email."""
+    task_instance = context['task_instance']
+    subject = f"Airflow Task Failure: {task_instance.task_id}"
+    email = EmailOperator(
+        task_id='send_failure_email',
+        to='anirudhak881@gmail.com',
+        subject=subject,
+        html_content=f"Task {task_instance.task_id} failed in DAG {context['dag'].dag_id}",
+    )
+    return email.execute(context=context)
+
+def success_email_notification(context):
+    """Send success email."""
+    task_instance = context['task_instance']
+    subject = f"Airflow Task Success: {task_instance.task_id}"
+    email = EmailOperator(
+        task_id=f'send_success_email_{task_instance.task_id}',
+        to='anirudhak881@gmail.com',
+        subject=subject,
+        html_content=f"Task {task_instance.task_id} completed successfully in DAG {context['dag'].dag_id}",
+    )
+    return email.execute(context=context)
+
+
 default_args = {
     'owner': 'MLOPS',
     'start_date': datetime(2024, 10, 21),
@@ -43,54 +68,79 @@ dag = DAG(
 run_Prophet_train = PythonOperator(
     task_id = 'Prophet_model_training',
     python_callable = Training_prophet,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 run_Prophet_validation = PythonOperator(
     task_id = 'Prophet_model_validation',
     python_callable = Validation_prophet,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 
 run_xgboost_Training = PythonOperator(
     task_id = 'XGBoost_model_Train',
     python_callable = Training_xgboost,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 
 run_xgboost_Validation = PythonOperator(
     task_id = 'XGBoost_model_Validation',
     python_callable = Validation_xgboost,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 
 run_random_forest_Training = PythonOperator(
     task_id = 'Random_Forest_model_Train',
     python_callable = Training_randomforest,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 
 run_random_forest_validation = PythonOperator(
     task_id = 'Random_Forest_model_Validation',
     python_callable = Validation_randomforest,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 
 run_bestmodel = PythonOperator(
     task_id = 'Best_Model',
     python_callable = bestmodel,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
     dag=dag
 )
 
 run_bias_check = PythonOperator(
     task_id = 'Bias_Model',
     python_callable = Bias_check,
+    on_failure_callback=failure_email_notification,
+    on_success_callback=success_email_notification,
+    dag=dag
+)
+
+notify_completion = EmailOperator(
+    task_id='send_email_on_pipeline_completion',
+    to='anirudhak881@gmail.com',  # Replace with your email
+    subject='Airflow Pipeline Completed',
+    html_content="The model pipeline has completed successfully.",
     dag=dag
 )
 
 run_Prophet_train >> run_Prophet_validation
 run_xgboost_Training >> run_xgboost_Validation
 run_random_forest_Training >> run_random_forest_validation
-[run_Prophet_validation, run_xgboost_Validation, run_random_forest_validation] >> run_bias_check >> run_bestmodel
+[run_Prophet_validation, run_xgboost_Validation, run_random_forest_validation] \
+>> run_bias_check >> run_bestmodel >> notify_completion
 
 if __name__ == "__main__":
     dag.cli()
