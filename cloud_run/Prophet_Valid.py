e9911a12741da16d1ed1ac07f4719ed10643524c
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2024-11-22 20:03:08 -0500

    prophet_checking

--- cloud_run/Prophet_Valid.py
+++ cloud_run/Prophet_Valid.py
@@ -12,6 +12,94 @@ import mlflow.prophet
 import mlflow.pyfunc
 import time
 import shap
+from google.cloud import storage
+import io
+from io import BytesIO
+import pickle5 as pickle
+import numpy as np
+from scipy import stats
+
+
+def setup_mlflow_tracking():
+    MLFLOW_TRACKING_DIR = os.environ.get('MLFLOW_TRACKING_DIR', '/app/mlruns')
+    os.makedirs(MLFLOW_TRACKING_DIR, exist_ok=True)
+    mlflow.set_tracking_uri(f"file://{MLFLOW_TRACKING_DIR}")
+
+
+class DataFeatureEngineer:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = None
+        self.fitting_lambda = None
+    
+    def get_lambda(self):
+        return self.fitting_lambda
+
+    def load_data(self):
+
+        client = storage.Client()
+
+        bucket_name = 'airquality-mlops-rg'
+
+        # Get the bucket and the blob (file)
+        bucket = client.bucket(bucket_name)
+
+        blob_name = os.path.join(self.file_path)
+        blob = bucket.blob(blob_name)
+        pickle_data = blob.download_as_bytes() 
+        self.data = pickle.load(BytesIO(pickle_data))
+        print(f"Data loaded from {self.file_path}.")
+    
+    def handle_skewness(self, column_name='pm25'):
+        skewness = self.data[column_name].skew()
+        print(f'Original Skewness: {skewness}')
+        if np.abs(skewness)<0.5:
+            return column_name
+        else:
+            self.data[f'{column_name}_log'] = np.log1p(self.data[column_name])
+            log_skewness = self.data[f'{column_name}_log'].skew()
+            print(f'Log Transformed Skewness: {log_skewness}')
+
+            self.data[f'{column_name}_boxcox'], self.fitting_lambda = stats.boxcox(self.data[column_name] + 1)
+            boxcox_skewness = self.data[f'{column_name}_boxcox'].skew()
+            print(f'Box-Cox Transformed Skewness: {boxcox_skewness}')
+
+            if abs(boxcox_skewness) < abs(log_skewness):
+                self.data.drop(columns=[f'{column_name}_log'],inplace=True)
+                print("Choosing Box-Cox transformed column.")
+                return f'{column_name}_boxcox'
+            else:
+                print("Choosing Log transformed column.")
+                self.data.drop(columns=[f'{column_name}_boxcox'],inplace=True)
+                return f'{column_name}_log'
+
+    def feature_engineering(self, chosen_column):
+        # Create lag features
+        for lag in range(1, 6):  # Creates lag_1 to lag_5
+            self.data[f'lag_{lag}'] = self.data[chosen_column].shift(lag)
+
+        self.data['rolling_mean_3'] = self.data[chosen_column].rolling(window=3).mean()
+        self.data['rolling_mean_6'] = self.data[chosen_column].rolling(window=6).mean()
+        self.data['rolling_mean_24'] = self.data[chosen_column].rolling(window=24).mean()
+        self.data['rolling_std_3'] = self.data[chosen_column].rolling(window=3).std()
+        self.data['rolling_std_6'] = self.data[chosen_column].rolling(window=6).std()
+        self.data['rolling_std_24'] = self.data[chosen_column].rolling(window=24).std()
+        self.data['ema_3'] = self.data[chosen_column].ewm(span=3, adjust=False).mean()
+        self.data['ema_6'] = self.data[chosen_column].ewm(span=6, adjust=False).mean()
+        self.data['ema_24'] = self.data[chosen_column].ewm(span=24, adjust=False).mean()
+        self.data['diff_1'] = self.data[chosen_column].diff(1)
+        self.data['diff_2'] = self.data[chosen_column].diff(2)
+        self.data['hour'] = self.data.index.hour
+        self.data['day_of_week'] = self.data.index.dayofweek
+        self.data['day_of_year'] = self.data.index.dayofyear
+        self.data['month'] = self.data.index.month
+        self.data['sin_hour'] = np.sin(2 * np.pi * self.data['hour'] / 24)
+        self.data['cos_hour'] = np.cos(2 * np.pi * self.data['hour'] / 24)
+        self.data['sin_day_of_week'] = np.sin(2 * np.pi * self.data['day_of_week'] / 7)
+        self.data['cos_day_of_week'] = np.cos(2 * np.pi * self.data['day_of_week'] / 7)
+        self.data.dropna(inplace=True)
+        print("Feature engineering completed and NaN values dropped.")
+
 
 
 class ProphetWrapper(mlflow.pyfunc.PythonModel):
@@ -29,8 +117,24 @@ class ProphetPM25Model:
         self.lambda_value = lambda_value
         self.model_save_path = model_save_path
         self.model = Prophet()
-        with open(self.model_save_path, 'rb') as f:
-            self.model = pickle.load(f)
+
+        bucket_name = "airquality-mlops-rg"
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(self.model_save_path)
+        temp_file_name = "/tmp/temp_model.pth"
+        # Download the file from the bucket to the temporary path
+        blob.download_to_filename(temp_file_name)
+
+        try:
+            with open(temp_file_name, 'rb') as f:
+                self.model = pd.read_pickle(f)
+            print("Model loaded successfully using pandas.read_pickle.")
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            self.model = None
+        # with open(self.model_save_path, 'rb') as f:
+        #     self.model = pickle.load(f)
         self.X_train = None
         self.y_train = None
         self.X_test = None
@@ -39,9 +143,27 @@ class ProphetPM25Model:
         self.y_test_original = None
     
     def load_data(self):
-        # Load training and test data
-        train_data = pd.read_pickle(self.train_file)
-        test_data = pd.read_pickle(self.test_file)
+        client = storage.Client()
+
+        bucket_name = 'airquality-mlops-rg'
+        pickle_file_path = 'processed/train/feature_eng_train_data.pkl'
+        pickle_file_path_test = 'processed/test/feature_eng_test_data.pkl'
+
+        # Get the bucket and the blob (file)
+        bucket = client.bucket(bucket_name)
+
+        blob_name = os.path.join(pickle_file_path)
+        blob = bucket.blob(blob_name)
+        pickle_data = blob.download_as_bytes() 
+        train_data = pickle.load(BytesIO(pickle_data))
+
+        blob_name_test = os.path.join(pickle_file_path_test)
+        blob_test = bucket.blob(blob_name_test)
+        pickle_data_test = blob_test.download_as_bytes() 
+        test_data = pickle.load(BytesIO(pickle_data_test))
+        # # Load training and test data
+        # train_data = pd.read_pickle(self.train_file)
+        # test_data = pd.read_pickle(self.test_file)
 
         # Extract Box-Cox transformed y and original y
         for column in train_data.columns:
@@ -59,6 +181,9 @@ class ProphetPM25Model:
         self.X_test = test_data.drop(columns=['pm25'])
 
     def shap_analysis(self):
+        storage_client = storage.Client()
+        bucket_name = "airquality-mlops-rg"
+        bucket = storage_client.bucket(bucket_name)
         # Custom predict function for SHAP that works with Prophet
         def prophet_predict(df):
             future = pd.DataFrame({'ds': df['ds']})
@@ -74,6 +199,8 @@ class ProphetPM25Model:
 
         # Calculate SHAP values for the test set
         shap_values = explainer(shap_data)
+        artifact_dir = os.path.join(os.getcwd(), 'artifacts')
+        os.makedirs(artifact_dir, exist_ok=True)
 
         # Plot SHAP summary and save it as an artifact
         shap.summary_plot(shap_values, shap_data, show=False)
@@ -81,6 +208,10 @@ class ProphetPM25Model:
         plt.savefig(shap_plot_path)
         mlflow.log_artifact(shap_plot_path)
         print(f"SHAP summary plot saved at {shap_plot_path}")
+
+        destination_blob_name = f'artifacts/shap_summary_plot_randomforest.png'
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(shap_plot_path)
 
     def preprocess_data(self):
         # Prepare training data in Prophet format
@@ -114,12 +245,31 @@ class ProphetPM25Model:
         return y_pred_original_valid
 
     def load_weights(self):
-        # Load the Prophet model from the specified path
-        with open(self.model_save_path, 'rb') as f:
-            self.model = pickle.load(f)
-        print(f"Model loaded from {self.model_save_path}")
+        bucket_name = "airquality-mlops-rg"
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(self.model_save_path)
+
+        temp_file_name = "/tmp/temp_model.pth"
+        # Download the file from the bucket to the temporary path
+        blob.download_to_filename(temp_file_name)
+
+        try:
+            with open(temp_file_name, 'rb') as f:
+                self.model = pd.read_pickle(f)
+            print("Model loaded successfully using pandas.read_pickle.")
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            self.model = None
+        # # Load the Prophet model from the specified path
+        # with open(self.model_save_path, 'rb') as f:
+        #     self.model = pickle.load(f)
+        # print(f"Model loaded from {self.model_save_path}")
 
     def plot_results(self, y_pred_original_valid):
+        storage_client = storage.Client()
+        bucket_name = "airquality-mlops-rg"
+        bucket = storage_client.bucket(bucket_name)
         # Plot actual vs predicted values
         plt.figure(figsize=(10,6))
         plt.plot(self.y_test_original.values, label='Actual PM2.5', color='blue')
@@ -129,22 +279,27 @@ class ProphetPM25Model:
         plt.ylabel('PM2.5 Value')
         plt.legend()
 
+        artifact_dir = os.path.join(os.getcwd(), 'artifacts')
+        os.makedirs(artifact_dir, exist_ok=True)
+
         # Save the plot as a PNG file
         plot_path = os.path.join(os.getcwd(), 'artifacts/pm25_actual_vs_predicted_Prophet.png')
         plt.savefig(plot_path)
         mlflow.log_artifact(plot_path)
         print(f"Plot saved at {plot_path}")
+        destination_blob_name = f'pm25_actual_vs_predicted_RandomForest.png'
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(plot_path)
 # Main function to orchestrate the workflow
 def main():
+    setup_mlflow_tracking()
     mlflow.set_experiment("PM2.5 Prophet")
-    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file:///app/mlruns"))
-    curr_dir = os.getcwd()
-    print(curr_dir)
-    data_prepocessing_path_pkl = os.path.join(curr_dir,'DataPreprocessing/src/data_store_pkl_files')
-    # Step 1: Load Data using DataFeatureEngineer
-    file_path = os.path.join(data_prepocessing_path_pkl, 'test_data/no_anamoly_test_data.pkl')
-    sys.path.append(data_prepocessing_path_pkl)
-    from DataPreprocessing.src.test.data_preprocessing.feature_eng import DataFeatureEngineer
+    
+    bucket_name = "airquality-mlops-rg"
+    train_file_gcs = f'processed/train/feature_eng_train_data.pkl'
+    test_file_gcs = f'processed/test/feature_eng_test_data.pkl'
+    model_save_path_gcs = f'weights/prophet_pm25_model.pth'
+    file_path = f'processed/test/no_anamoly_test_data.pkl'
     engineer = DataFeatureEngineer(file_path)
     engineer.load_data()
     chosen_column = engineer.handle_skewness(column_name='pm25')
@@ -152,16 +307,11 @@ def main():
     fitting_lambda = engineer.get_lambda()
     mlflow.log_param("lambda_value", fitting_lambda)
 
-    # Step 2: Define file paths
-    train_file = os.path.join(data_prepocessing_path_pkl, 'train_data/feature_eng_train_data.pkl')
-    test_file = os.path.join(data_prepocessing_path_pkl, 'test_data/feature_eng_test_data.pkl')
-    model_save_path = os.path.join(curr_dir, 'weights/prophet_pm25_model.pth')
-    
     if mlflow.active_run():
         mlflow.end_run()
     
     with mlflow.start_run() as run:
-        prophet_model = ProphetPM25Model(train_file, test_file, fitting_lambda, model_save_path)
+        prophet_model = ProphetPM25Model(train_file_gcs, test_file_gcs, fitting_lambda, model_save_path_gcs)
         prophet_model.load_data()
         prophet_model.preprocess_data()
         y_pred_original = prophet_model.evaluate()
